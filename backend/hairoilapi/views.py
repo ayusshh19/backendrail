02e7b13d0551c45f5c92668352bad5c4914e8932
ayussh19 <cm.b.49ayush.shukla@gmail.com>
2023-03-14 12:28:01 +0530

    done

--- backend/hairoilapi/views.py
+++ backend/hairoilapi/views.py
@@ -118,12 +118,12 @@ def adminpanel(request):
 def purchasecompletion(request):
     if request.method=='GET':
         return Response({'msg':'Complete product purchase '},status=status.HTTP_200_OK)
-    
+
     if request.method=='POST':
-        username=request.data['username']
-        userobj=Userregister.objects.filter(username=username)
-        product=Productpurchase.objects.filter(userid=userobj).update(paymentcompletion=True)
-        productserializer=Productpurchaseserializer(product,many=True)
+        prodid=request.data['prodid']
+        product=Productpurchase.objects.filter(id=prodid).update(paymentcompletion=True)
+        prodobj=Productpurchase.objects.filter(id=prodid)
+        productserializer=Productpurchaseserializer(prodobj,many=True)
         return Response({'msg':productserializer.data},status=status.HTTP_200_OK)
 
 @api_view(['GET','POST'])
