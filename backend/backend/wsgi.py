2c99bc58cac28a08b33dc4d1e558d871376d9964
ayussh19 <cm.b.49ayush.shukla@gmail.com>
2023-03-15 19:19:35 +0530

    new

--- backend/backend/wsgi.py
+++ backend/backend/wsgi.py
@@ -8,9 +8,13 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
-
+from django.contrib.auth.models import User
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 application = get_wsgi_application()
+
+users = User.objects.all()
+if not users:
+    User.objects.create_superuser(username="ayushshukla", email="cm.b.49ayush.shukla@gmail.com", password="1234", is_active=True, is_staff=True)
